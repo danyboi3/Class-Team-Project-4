ab270438e491dda4a97bfeb88544a124a4211ce7
Michael Grinspan <mike.grinspan@gmail.com>
2017-10-29 13:51:59 -0400

    Reformatted code

--- src/main/java/edu/cuny/brooklyn/cisc3120/project/game/TargetGame.java
+++ src/main/java/edu/cuny/brooklyn/cisc3120/project/game/TargetGame.java
@@ -1,58 +1,57 @@
 package edu.cuny.brooklyn.cisc3120.project.game;
 
-import java.util.Random;
-
+import edu.cuny.brooklyn.cisc3120.project.game.gui.GameUI;
+import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import edu.cuny.brooklyn.cisc3120.project.game.gui.GameUI;
-import javafx.stage.Stage;
+import java.util.Random;
 
 public class TargetGame {
-    private static Logger logger = LoggerFactory.getLogger(TargetGame.class);
-    
-    private final static char TARGET_INDICATOR_ON_BOARD = 'X';
+	private final static char TARGET_INDICATOR_ON_BOARD = 'X';
+	private static Logger logger = LoggerFactory.getLogger(TargetGame.class);
+	private final int GAME_TARGET_AREA_WIDTH = 40;
+	private final int GAME_TARGET_AREA_HEIGHT = 40;
 
-    private final int GAME_TARGET_AREA_WIDTH = 40;
-    private final int GAME_TARGET_AREA_HEIGHT = 40;
+	private GameBoard gameBoard; // having its own dimension: cells
+	private GameUI gameUI; // having its own dimension: cells to characters
+	private Random rng;
+	private Target target;
 
-    private GameBoard gameBoard; // having its own dimension: cells
-    private GameUI gameUI; // having its own dimension: cells to characters
-    private Random rng;
-    private Target target;
-    
-    public interface PostShotAction {
-        public void postAction();
-    }
-
-    public TargetGame(Stage primaryStage) {
-        gameBoard = new GameBoard(GAME_TARGET_AREA_HEIGHT, GAME_TARGET_AREA_WIDTH);
-        gameUI = new GameUI(gameBoard, primaryStage);
-        rng = new Random();
-    }
-
-    public void play() {
-        setNewTarget();
-	    gameUI.setPostShotActionHandler(() -> {setNewTarget();});
-        gameUI.show();	    
+	public TargetGame(Stage primaryStage) {
+		gameBoard = new GameBoard(GAME_TARGET_AREA_HEIGHT, GAME_TARGET_AREA_WIDTH);
+		gameUI = new GameUI(gameBoard, primaryStage);
+		rng = new Random();
 	}
 
-    private void setNewTarget() {
-        target = getRandomTarget();
-        addTargetToBoard(target);
-        gameUI.addTargetToUI(target);
-    }
+	public void play() {
+		setNewTarget();
+		gameUI.setPostShotActionHandler(() -> {
+			setNewTarget();
+		});
+		gameUI.show();
+	}
 
-    private Target getRandomTarget() {
-        int x = rng.nextInt(GAME_TARGET_AREA_WIDTH);
-        int y = rng.nextInt(GAME_TARGET_AREA_HEIGHT);
-        Target target = new Target(x, y);
-        logger.debug("Target: " + x + "," + y);
-        return target;
-    }
+	private void setNewTarget() {
+		target = getRandomTarget();
+		addTargetToBoard(target);
+		gameUI.addTargetToUI(target);
+	}
 
-    private void addTargetToBoard(Target target) {
-        gameBoard.setCell(target.getX(), target.getY(), TARGET_INDICATOR_ON_BOARD);
-    }
+	private Target getRandomTarget() {
+		int x = rng.nextInt(GAME_TARGET_AREA_WIDTH);
+		int y = rng.nextInt(GAME_TARGET_AREA_HEIGHT);
+		Target target = new Target(x, y);
+		logger.debug("Target: " + x + "," + y);
+		return target;
+	}
+
+	private void addTargetToBoard(Target target) {
+		gameBoard.setCell(target.getX(), target.getY(), TARGET_INDICATOR_ON_BOARD);
+	}
+
+	public interface PostShotAction {
+		public void postAction();
+	}
 
 }
--- src/main/java/edu/cuny/brooklyn/cisc3120/project/game/app/TargetGameApp.java
+++ src/main/java/edu/cuny/brooklyn/cisc3120/project/game/app/TargetGameApp.java
@@ -4,17 +4,15 @@ import edu.cuny.brooklyn.cisc3120.project.game.TargetGame;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class TargetGameApp extends Application
-{
-    public static void main( String[] args )
-    {
-        launch(args);
-    }
-    
-    @Override
-    public void start(Stage primaryStage) throws Exception {
+public class TargetGameApp extends Application {
+	public static void main(String[] args) {
+		launch(args);
+	}
 
-        TargetGame game = new TargetGame(primaryStage);
-        game.play();
-    }
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		TargetGame game = new TargetGame(primaryStage);
+		game.play();
+	}
 }
